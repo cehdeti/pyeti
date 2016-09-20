f4c235bcdab181c2c20788ee12e052241898bcd5
Thomas Saunders <saun0063@umn.edu>
2016-09-20 11:47:52 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='awesome',
+      version='0.1',
+      description='The awesomest Python package',
+      url='http://github.com/cehdeti/awesome',
+      author='ETI',
+      author_email='saun0063@umn.edu',
+      license='MIT',
+      packages=['awesome'],
+      zip_safe=False)
