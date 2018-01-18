59a64a014b7e158fc67d6af60ee8e544123ed462
Jake Bell <sigterm01@gmail.com>
2018-01-18 10:54:33 -0600

    Moving `typecast_from_field` to `eti_django` module

---
+++ pyeti/eti_django/utils.py
@@ -0,0 +1,33 @@
+try:
+    from django.utils import six
+except ImportError:
+    import six
+
+import dateutil.parser
+
+
+def typecast_from_field(field, value):
+    """
+    Given a Django model field and a value, typecast the value to the datatype
+    expected by the field.
+    """
+    if not isinstance(value, six.string_types):
+        return value
+    if field.deconstruct()[-1].get('null') and value.strip() == '':
+        return None
+
+    field_type = field.get_internal_type()
+    if field_type == 'BooleanField':
+        return is_truthy(value)
+    if field_type == 'CharField':
+        return value.strip()
+    if field_type == 'DateField':
+        return dateutil.parser.parse(value)
+    if field_type == 'FloatField':
+        return float(value)
+    if field_type == 'IntegerField':
+        return int(value)
+
+    return value
+
+
--- pyeti/utils.py
+++ pyeti/utils.py
@@ -4,7 +4,6 @@ except ImportError:
     import six
 
 import re
-import dateutil.parser
 
 
 def ignore_exception(*exception_classes):
@@ -47,31 +46,6 @@ def is_truthy(value):
             return False
 
     return bool(value)
-
-
-def typecast_from_field(field, value):
-    """
-    Given a Django model field and a value, typecast the value to the datatype
-    expected by the field.
-    """
-    if not isinstance(value, six.string_types):
-        return value
-    if field.deconstruct()[-1].get('null') and value.strip() == '':
-        return None
-
-    field_type = field.get_internal_type()
-    if field_type == 'BooleanField':
-        return is_truthy(value)
-    if field_type == 'CharField':
-        return value.strip()
-    if field_type == 'DateField':
-        return dateutil.parser.parse(value)
-    if field_type == 'FloatField':
-        return float(value)
-    if field_type == 'IntegerField':
-        return int(value)
-
-    return value
 
 
 _integer_re = re.compile('^\-?[\d,]*$')
