6f3f6acfb94e2d5ef853387394a77af9fd236af1
Mon Sucher <msucher@umn.edu>
2019-06-18 08:37:10 -0500

    Django 2 compatibility

--- pyeti/eti_django/support/views.py
+++ pyeti/eti_django/support/views.py
@@ -10,13 +10,18 @@ class SupportView(SuccessMessageMixin, FormView):
     template_name = 'eti_django/support/index.html'
     success_message = _('Your support request has been submitted. You should hear back from us shortly!')
 
+    def is_authenticated(user):
+        if callable(user.is_authenticated):
+            return user.is_authenticated()
+        return user.is_authenticated
+
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
 
     def get_initial(self):
         initial = super().get_initial()
-        if self.request.user.is_authenticated() and hasattr(self.request.user, 'email'):
+        if self.is_authenticated(self.request.user) and hasattr(self.request.user, 'email'):
             initial['email'] = self.request.user.email
         return initial
 
