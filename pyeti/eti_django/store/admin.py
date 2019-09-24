afbe153369bffa4e604f28b84a9a8308c1e919ef
Jake Bell <sigterm01@gmail.com>
2019-09-24 15:54:45 -0500

    Removing "delete selected" action from usage license admin

--- pyeti/eti_django/store/admin.py
+++ pyeti/eti_django/store/admin.py
@@ -65,6 +65,12 @@ class UsageLicenseAdmin(admin.ModelAdmin):
         """
         raise PermissionDenied('Usage licenses cannot be edited')
 
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
     def sync_from_store(self, request, queryset):
         success = 0
         failure = 0
